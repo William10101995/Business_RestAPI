843c8e12340506b601d619d26e8e4a91f96dfdc5
unknown <lopezwilliam177@gmail.com>
2021-04-20 11:00:55 -0300

    Adding Routes

--- API/src/routers/table.controller.ts
+++ API/src/routers/table.controller.ts
@@ -1,10 +1,6 @@
 
 import {RequestHandler} from 'express'
 
-//Testing
-export const getIni: RequestHandler = (req, res)=>{
-    res.send('Hola Mundo')
-}
 
 //Obtener Mesas
 export const getTable: RequestHandler = (req, res)=>{
@@ -16,15 +12,17 @@ export const createTable: RequestHandler = (req, res)=>{
     res.send('Mesa Creada')
 }
 
+//Actualizar una mesa. Aqui tendriamos el ingreso y egreso de usuarios
+export const updateTable: RequestHandler = (req, res)=>{
+    res.send('Mesa Actualizada')
+}
+
 //Borrar una mesa
 export const deleteTable: RequestHandler = (req, res)=>{
     res.send('Mesa Borrada')
 }
 
-//Actualizar una mesa. Aqui tendriamos el ingreso y egreso de usuarios
-export const updateTable: RequestHandler = (req, res)=>{
-    res.send('Mesa Actualizada')
-}
+
 
 //Obtener Usuarios
 export const getUsers: RequestHandler = (req, res)=>{
--- API/src/routers/table.routes.ts
+++ API/src/routers/table.routes.ts
@@ -1,5 +1,6 @@
 //Import Routers
 import {Router} from 'express'
+import { table } from 'node:console';
 //Import fuctions Routes
 import * as tableCtrl from './table.controller'
 //Metodh Router
@@ -7,7 +8,20 @@ const router = Router();
 
 
 //Routes
-router.get('/',tableCtrl.getIni)
+//GET: Obtener
+router.get('/tables',tableCtrl.getTable); //Obtener Mesas
+router.get('/users',tableCtrl.getUsers); //Obtener Usuarios
+
+//POST: Agregar
+router.post('/newtable', tableCtrl.createTable); //Alta de Mesa
+router.post('/newuser', tableCtrl.createUser); //Alta de Usuario
+
+//PUT: Actualizacion
+router.put('/tables/:id', tableCtrl.updateTable); //Actualizar Mesa
+router.put('/users/:id', tableCtrl.updateTable); //Actualizar Mesa
+
+
+
 
 export default  router;
 
